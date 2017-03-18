ddc7ed608fce72e0ddcaea287733e94f8d700190
Bryan Wegman <xxbw@icloud.com>
2017-03-18 14:09:31 -0700

    Add button to advance one generation at a time.

--- src/GameControls.js
+++ src/GameControls.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 class GameControls extends React.Component {
+
+
 	render() {
 	  return( 
 	    <div className="game--controls">
-	      <button>start</button>
+	      <button onClick={this.props.setGameToNextStageOfLife}>DEV: next</button>
+	      <button>start</button>	
 	      <button>stop</button>
 	      <button>clear</button>
 	      <label>Rows<input type="text" /></label>
