68adc0322ebfb7975a08ace13af6c56737748d6f
Bryan Wegman <xxbw@icloud.com>
2017-03-20 21:34:10 -0700

    Add next-button id to the next button.

--- src/GameControls.js
+++ src/GameControls.js
@@ -6,7 +6,7 @@ class GameControls extends React.Component {
 	render() {
 	  return( 
 	    <div className="game--controls">
-	      <button onClick={this.props.setGameToNextStageOfLife}>DEV: next</button>
+	      <button id='next-button' onClick={this.props.setGameToNextStageOfLife}>DEV: next</button>
 	      <button onClick={this.props.toggleGameState}>{this.props.gameIsRunning ? 'Stop' : 'Start'}</button>
 	      <button onClick={this.props.clearGrid}>Clear the Board</button>
 	      <label>Rows<input type="text" /></label>
